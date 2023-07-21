3dc1e1fc84051613025f57ea01ca1b49bdc078fe
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-07-21 10:24:49 +0700

    fix: seek

--- examples/stack/main.go
+++ examples/stack/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 
-	stack "github.com/truongnhatanh7/go-adt/types/stack"
+	stack "github.com/truongnhatanh7/go-adt/stack"
 )
 
 func main() {
 	s := stack.Stack{}
-  
+
 	s.Push(1)
 	s.Push(2)
+
+  fmt.Println(s.Seek())
   
   for !s.IsEmpty() {
     fmt.Println(s.Pop())
